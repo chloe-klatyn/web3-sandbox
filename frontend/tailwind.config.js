fafd7f8ca2c541cf05e6f04310962f3cc3ca3d49
chloet <chloe.t@krustuniverse.com>
2022-03-03 18:00:16 +0800

    added klaytn colours to config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: "#e5dede",
+        lightorange: "#ff8c00",
+        orange: "#ff2f00",
+        magma: "#850000",
+        umber: "#30211c",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
