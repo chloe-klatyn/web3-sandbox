34c43bfbd51898db8bbae4210def585ebd643bc9
chloet <chloe.t@krustuniverse.com>
2022-03-08 18:32:19 +0800

    added navigation row to toggle between pages

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -80,26 +80,24 @@ const Header = () => {
     }
   }
 
-  //   const getKaikasBalance = async () => {
-  //     const caver = new Caver(klaytnProvider)
-  //     const account = klaytnProvider.selectedAddress
-  //     const balance = await caver.klay.getBalance(account)
-  //     console.log('balance: ', balance)
-  //   }
-
   const getMetamaskBalance = async () => {
     let web3 = new Web3(ethProvider)
     const account = await ethereum.request({ method: 'eth_accounts' })
-    console.log('account: ', account)
     const balance = await window.ethereum.request({
       method: 'eth_getBalance',
       params: [account[0], 'latest'],
     })
     const wei = web3.utils.hexToNumberString(balance)
     const ether = web3.utils.fromWei(wei, 'ether')
-    console.log('balance: ', ether)
     setMetamaskBalace(ether)
   }
+
+  //   const getKaikasBalance = async () => {
+  //     const caver = new Caver(klaytnProvider)
+  //     const account = klaytnProvider.selectedAddress
+  //     const balance = await caver.klay.getBalance(account)
+  //     console.log('balance: ', balance)
+  //   }
 
   useEffect(() => {
     if (ethProvider && klaytnProvider) {
@@ -109,49 +107,54 @@ const Header = () => {
       klaytnProvider.on('networkChanged', function () {
         detectNetwork()
       })
-      //   getKaikasBalance()
       getMetamaskBalance()
+      // getKaikasBalance()
     }
   }, [ethProvider, klaytnProvider])
 
   return (
-    <header className="flex place-content-between p-6 items-center text-gray-900 bg-gray-100 shadow shadow-md">
-      <WalletModal walletModal={walletModal} setWalletModal={setWalletModal} />
-      <Link href="/">
-        <a className="mx-10">Klaytn Kit</a>
-      </Link>
-      <ul>
-        <li className="mx-10">
-          <Link href="/dashboard">
-            <a>Dashboard</a>
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex items-right">
-        <div className="flex justify-center items-center">
-          <div className="mx-6">{metamaskBalance} KLAY</div>
-          <div className="xl:w-84">
-            <select
-              className="form-select block w-full px-2 py-2 font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded-full transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none"
-              aria-label="Default select example"
-              value={network}
-              onChange={changeNetwork}
-            >
-              {networks.map((env) => (
-                <option key={env}>{env}</option>
-              ))}
-            </select>
+    <header className="grid grid-rows-2">
+      <div className="flex place-content-between p-3 items-center text-gray-900 bg-gray-100">
+        <WalletModal walletModal={walletModal} setWalletModal={setWalletModal} />
+        <Link href="/">
+          <a className="mx-10">Klaytn Kit</a>
+        </Link>
+
+        <ul className="flex items-right">
+          <div className="flex justify-center items-center">
+            <div className="mx-6">{metamaskBalance} KLAY</div>
+            <div className="xl:w-84">
+              <select
+                className="form-select block w-full px-2 py-2 font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded-full transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none"
+                aria-label="Default select example"
+                value={network}
+                onChange={changeNetwork}
+              >
+                {networks.map((env) => (
+                  <option key={env}>{env}</option>
+                ))}
+              </select>
+            </div>
+            <li className="mx-8">
+              <button
+                className="rounded-full bg-blue-600 px-3 py-2 text-white"
+                onClick={() => setWalletModal(true)}
+              >
+                Connect
+              </button>
+            </li>
           </div>
-          <li className="mx-8">
-            <button
-              className="rounded-full bg-blue-600 px-3 py-2 text-white"
-              onClick={() => setWalletModal(true)}
-            >
-              Connect
-            </button>
+        </ul>
+      </div>
+      <div className="flex place-content-center items-center text-gray-900 bg-gray-100 shadow">
+        <ul>
+          <li className="mx-10 hover:text-blue-600 hover:underline hover:underline-offset-4 focus:bg-red-500 ">
+            <Link href="/dashboard">
+              <a>Dashboard</a>
+            </Link>
           </li>
-        </div>
-      </ul>
+        </ul>
+      </div>
     </header>
   )
 }
