4287f4091b71cb6947a9e1afc0a107bedc681b6c
chloet <chloe.t@krustuniverse.com>
2022-03-10 16:09:17 +0800

    adjust border colours and code icons

--- frontend/components/InfoCard.tsx
+++ frontend/components/InfoCard.tsx
@@ -1,10 +1,10 @@
-import { ChevronDoubleRightIcon } from '@heroicons/react/outline'
+import { CodeIcon } from '@heroicons/react/outline'
 
 const InfoCard = () => {
   return (
     <div className="w-screen">
-      <div className="border shadow shadow-lg w-1/5 rounded-md border-2 border-gray-400 border-double p-4">
-        <div className="border-b-2 border-dotted text-xl pb-2 hover:text-gray-500">
+      <div className="border shadow shadow-lg w-1/5 rounded-md border-2 border-grey border-dotted p-4">
+        <div className="border-b-2 border-dotted border-grey text-xl pb-2 hover:text-gray-500">
           <a href="https://github.com/Krustuniverse-KlaytnGroup/klaytn-contracts" target="_blank">
             DApp Title
           </a>
@@ -16,7 +16,7 @@ const InfoCard = () => {
         <div className="flex place-content-between">
           <span>xxx</span>
           <a href="https://github.com/Krustuniverse-KlaytnGroup/klaytn-contracts" target="_blank">
-            <ChevronDoubleRightIcon className="w-5 h-5 ml-2 text-lightorange cursor-pointer transition duration-500 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110" />
+            <CodeIcon className="w-6 h-7 text-umber cursor-pointer transition duration-500 ease-in-out hover:translate-y-1 hover:scale-90" />
           </a>
         </div>
       </div>
