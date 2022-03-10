cdb5478ee0920133ab82cec075fdf5329d2194ac
chloet <chloe.t@krustuniverse.com>
2022-03-10 16:02:54 +0800

    added transitions to infocard and subheader

--- frontend/components/InfoCard.tsx
+++ frontend/components/InfoCard.tsx
@@ -1,13 +1,24 @@
+import { ChevronDoubleRightIcon } from '@heroicons/react/outline'
+
 const InfoCard = () => {
   return (
     <div className="w-screen">
-      <div className="border shadow shadow-md w-1/5 rounded-md border-4 border-gray-700">
-        <div>
+      <div className="border shadow shadow-lg w-1/5 rounded-md border-2 border-gray-400 border-double p-4">
+        <div className="border-b-2 border-dotted text-xl pb-2 hover:text-gray-500">
           <a href="https://github.com/Krustuniverse-KlaytnGroup/klaytn-contracts" target="_blank">
             DApp Title
           </a>
         </div>
-        <div>Description</div>
+        <div className="py-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua
+        </div>
+        <div className="flex place-content-between">
+          <span>xxx</span>
+          <a href="https://github.com/Krustuniverse-KlaytnGroup/klaytn-contracts" target="_blank">
+            <ChevronDoubleRightIcon className="w-5 h-5 ml-2 text-lightorange cursor-pointer transition duration-500 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110" />
+          </a>
+        </div>
       </div>
     </div>
   )
--- frontend/components/Subheader.tsx
+++ frontend/components/Subheader.tsx
@@ -5,17 +5,17 @@ const Subheader = () => {
     <div className="flex place-content-center items-center text-gray-900 bg-gray-100 shadow">
       <div className="flex text-xl">
         <Link href="/dashboard">
-          <button className="mx-10 transition ease-in-out delay-100 hover:text-blue-600 hover:border-b-2 hover:border-blue-500 focus:border-blue-500 focus:text-blue-600 focus:border-b-2 pb-2 border-b-2 border-gray-100">
+          <button className="mx-10 transition ease-in-out duration-400 delay-100 hover:-translate-y-1 hover:scale-110 hover:text-blue-600 hover:border-b-2 hover:border-blue-500 focus:border-blue-500 focus:text-blue-600 focus:border-b-2 pb-2 border-b-2 border-gray-100">
             Dashboard
           </button>
         </Link>
         <Link href="/contracts">
-          <button className="mx-10 transition ease-in-out delay-100 hover:text-blue-600 hover:border-b-2 hover:border-blue-500 focus:border-blue-500 focus:text-blue-600 focus:border-b-2 pb-2 border-b-2 border-gray-100">
+          <button className="mx-10 transition ease-in-out duration-400 delay-100 hover:-translate-y-1 hover:scale-110  hover:text-blue-600 hover:border-b-2 hover:border-blue-500 focus:border-blue-500 focus:text-blue-600 focus:border-b-2 pb-2 border-b-2 border-gray-100">
             Contracts
           </button>
         </Link>
         <Link href="/transfers">
-          <button className="mx-10 transition ease-in-out delay-100 hover:text-blue-600 hover:border-b-2 hover:border-blue-500 focus:border-blue-500 focus:text-blue-600 focus:border-b-2 pb-2 border-b-2 border-gray-100">
+          <button className="mx-10 transition ease-in-out duration-400 delay-100 hover:-translate-y-1 hover:scale-110  hover:text-blue-600 hover:border-b-2 hover:border-blue-500 focus:border-blue-500 focus:text-blue-600 focus:border-b-2 pb-2 border-b-2 border-gray-100">
             Transfers
           </button>
         </Link>
