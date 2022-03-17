5b635d4999b17d32b985ea8a5302c3cfa39dd2bf
toniya-klaytn <toniya.sun@krustuniverse.com>
2022-03-17 12:03:35 +0800

    adding kip37 contracts

--- backend/migrations/2_contract_migration.js
+++ backend/migrations/2_contract_migration.js
@@ -1,31 +1,31 @@
 const fs = require('fs')
-var kip17Token = artifacts.require('KIP17Token');
+var kip37Token = artifacts.require('KIP37');
 
 
 
 module.exports = function(deployer) {
-  deployer.deploy(kip17Token, 'KlaytnNFT', 'NFT')
+  deployer.deploy(kip37Token, 'KlaytnNFT', 'NFT')
   .then(() => {
     // Record recently deployed contract address to 'deployedAddress' file.
-    if (kip17Token._json) {
+    if (kip37Token._json) {
       fs.mkdir('../frontend/deployed', { recursive: true }, (err) => {
         if (err) throw err;
       });
       // Save abi file to deployedABI.
       fs.writeFile(
         '../frontend/deployed/kip17TokenABI',
-        JSON.stringify(kip17Token._json.abi, 2),
+        JSON.stringify(kip37Token._json.abi, 2),
         (err) => {
           if (err) throw err
-          console.log(`The abi of ${kip17Token._json.contractName} is recorded on deployedABI file`)
+          console.log(`The abi of ${kip37Token._json.contractName} is recorded on deployedABI file`)
         })
       }
       fs.writeFile(
         '../frontend/deployed/kip17TokenAddress',
-        kip17Token.address,
+        kip37Token.address,
         (err) => {
           if (err) throw err
-          console.log(`The deployed contract address * ${kip17Token.address} * is recorded on deployedAddress file`)
+          console.log(`The deployed contract address * ${kip37Token.address} * is recorded on deployedAddress file`)
       })
     })
 }
